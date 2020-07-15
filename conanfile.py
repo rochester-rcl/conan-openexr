ee29cccc8a7e50dbfdd3ea70d0f3e45f4d505d59
jjromphf <jromphf@library.rochester.edu>
2020-07-15 14:43:46 -0400

    added conditional rpath handling

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class OpenEXRConan(ConanFile):
     name = "OpenEXR"
     version = "2.4.0"
     license = "BSD"
-    requires = ("zlib/1.2.8@conan/stable")
+    requires = "zlib/1.2.8@conan/stable"
     settings = "os", "compiler", "build_type", "arch"
     url = "https://github.com/rochester-rcl/conan-openexr"
     options = {"shared": [True, False], "namespace_versioning": [True, False]}
@@ -18,37 +18,54 @@ class OpenEXRConan(ConanFile):
 
     def source(self):
         tools.download(
-            "https://github.com/AcademySoftwareFoundation/openexr/archive/v{}.tar.gz".format(self.version),
-            "openexr.tar.gz"
+            "https://github.com/AcademySoftwareFoundation/openexr/archive/v{}.tar.gz".format(
+                self.version
+            ),
+            "openexr.tar.gz",
         )
-        tools.untargz('openexr.tar.gz')
-        os.unlink('openexr.tar.gz')
+        tools.untargz("openexr.tar.gz")
+        os.unlink("openexr.tar.gz")
         # self.run("cd openexr && git checkout release/2.3".format(self.version))
-        tools.replace_in_file("{}/openexr-{}/CMakeLists.txt".format(self.source_folder, self.version),
-                              "project(OpenEXRMetaProject)",
-                              """project(OpenEXRMetaProject)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-set (CMAKE_CXX_STANDARD 11)""")
+        tools.replace_in_file(
+            "{}/openexr-{}/CMakeLists.txt".format(self.source_folder, self.version),
+            "project(OpenEXRMetaProject)",
+            """project(OpenEXRMetaProject)
+               include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+               if (APPLE)
+                    conan_basic_setup(KEEP_RPATHS)
+               else()
+                    conan_basic_setup()
+               endif()
+               set (CMAKE_CXX_STANDARD 11)""",
+        )
 
     def build(self):
         cmake = CMake(self)
 
         cmake.definitions.update(
-            {"OPENEXR_BUILD_ILMBASE": True,
-             "OPENEXR_BUILD_OPENEXR": True,
-             "OPENEXR_BUILD_SHARED": self.options.shared,
-             "OPENEXR_BUILD_STATIC": not self.options.shared,
-             "OPENEXR_BUILD_PYTHON_LIBS": False,
-             "OPENEXR_NAMESPACE_VERSIONING": False,
-             "CMAKE_PREFIX_PATH": self.deps_cpp_info["zlib"].rootpath,
-             })
+            {
+                "OPENEXR_BUILD_ILMBASE": True,
+                "OPENEXR_BUILD_OPENEXR": True,
+                "OPENEXR_BUILD_SHARED": self.options.shared,
+                "OPENEXR_BUILD_STATIC": not self.options.shared,
+                "OPENEXR_BUILD_PYTHON_LIBS": False,
+                "OPENEXR_NAMESPACE_VERSIONING": False,
+                "CMAKE_PREFIX_PATH": self.deps_cpp_info["zlib"].rootpath,
+            }
+        )
 
-        cmake.configure(source_dir="{}/openexr-{}".format(self.source_folder, self.version))
+        cmake.configure(
+            source_dir="{}/openexr-{}".format(self.source_folder, self.version)
+        )
         cmake.build(target="install")
 
     def package(self):
-        self.copy("*.h", dst="include", src="package/include".format(self.version), keep_path=True)
+        self.copy(
+            "*.h",
+            dst="include",
+            src="package/include".format(self.version),
+            keep_path=True,
+        )
 
         self.copy("*.lib", dst="lib", src=".", keep_path=False)
         self.copy("*.a", dst="lib", src=".", keep_path=False)
@@ -63,9 +80,8 @@ set (CMAKE_CXX_STANDARD 11)""")
         if self.options.shared and self.settings.os == "Windows":
             self.cpp_info.defines.append("OPENEXR_DLL")
         self.cpp_info.bindirs = ["bin"]
-        self.cpp_info.includedirs = ['include', 'include/OpenEXR']
-        libs = ["IlmImf", "IlmImfUtil", "Imath", "IexMath", "Half", "Iex",
-                "IlmThread"]
+        self.cpp_info.includedirs = ["include", "include/OpenEXR"]
+        libs = ["IlmImf", "IlmImfUtil", "Imath", "IexMath", "Half", "Iex", "IlmThread"]
 
         def set_version(lib):
             return "{}-{}_{}".format(lib, major, minor)
